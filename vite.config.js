ef92f8258fb53dbbe4f8c604583ba028d4d8c28d
sergii <sk2009@gmail.com>
2023-09-16 00:04:14 +0300

    build fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
-  server: { port: 3000, hmr: { port: 3000 } }
+    plugins: [crx({ manifest })],
+    server: { port: 3000, hmr: { port: 3000 } },
+    build: {
+        target: 'esnext'
+    }
 })
